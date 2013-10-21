2264df5ff165a55065034a7e45cf73c5137b17ba
Alexandr <slipfan4eg2@yandex.ru>
2013-10-21 23:10:04 +0400

    228

--- dyplom/renameremove_column.rb
+++ dyplom/renameremove_column.rb
@@ -4,69 +4,14 @@ require './uniibrut'
 require './genereator'
 require './helper'
 require 'faker'
-def drop_table(table)
-    "\t\t def self.down\n\t\t\tdrop_table :#{table}\n\t\t end\n"
-end
 def addcolumn(hash,  name, table, data ) 
-  "\t\tadd_column :#{table}, :#{name}, :#{data}\n   "
+  "\t\t:#{table}, :#{name}, :#{data}\n   "
 end
-def removecolumn(hash, name, table, data ) 
- 
-  "\t\tremove_column :#{table}, :#{name}, :#{data}\n   "
-end
-
-def addremove(correct=true)
-
-  table_name = (random_column_name).to_s
-  drop_table_name = correct ? (random_false_column).to_s : table_name
-  indicator = correct ?  "F": "T"
-str = <<-END
-
-#{indicator}
-\\begin{verbatim}
-class CreateUsers < ActiveRecord::Migration
-  def change
-   create_table #{table_name} do |t
-     def self.up
-END
-   # add columns
-  data_types = []
-  column_names = []
-  number_columns = rand(4..6)
-  
-  number_columns.times do
-    files_with_types = correct ? 'words/falsedatatype.yaml' : 'qq.yaml'
-    hash = YAML::load(open(files_with_types))
-    data_type = hash.keys.shuffle.first
-    column_name =  (random_column_name).to_s
-	str << addcolumn(hash, column_name, table_name, data_type)
-	column_names << column_name
-	data_types << data_type
+def add_reference(class_name)
+  "\t\tadd_references :#{class_name}, index :#{["true","false"].shuffle.first}\n"
   end
-  str << <<-END
-   def self.down
-  END
-	number_columns.times do
-	column_name = correct ? " " : column_names.pop
-	data_type = data_types.pop
-	str << removecolumn(hash, column_name, table_name, data_type)
-  end	
-    str << <<-END
-	     end
-	  end 
-    end
-\\end{verbatim}
-  END
-end
-def rmrv
-text = "\nQ\nSelect true migration\n"
-res = []
-res <<  addremove(false)
-3.times { res <<  addremove }
-File.open('migration3.tex', 'a') do |file| 
-	file.puts text
-	file.puts res.shuffle.join("\n")
-end
+def removecolumn(hash, name, table, data ) 
+  "\t\tremove_column :#{table}, :#{name}, :#{data}\n   "
 end
 
 
@@ -76,29 +21,28 @@ def renamecolumn(hash, name, table, new_column_name )
 end
 
 def rename(correct=true)
-
+    files_with_types = correct ? 'words/falsedatatype.yaml' : 'qq.yaml'
+    hash = YAML::load(open(files_with_types))
   table_name = (random_column_name).to_s
   drop_table_name = correct ? (random_false_column).to_s : table_name
   indicator = correct ?  "F": "T"
+  class_name = ["users","dogs","persons"].shuffle.first
+  class_names = []
+  class_names << class_name
 str = <<-END
-
 #{indicator}
 \\begin{verbatim}
-  class CreateUsers < ActiveRecord::Migration
-    def change
-     create_table #{table_name} do |t
-	  def self.up
+  class Create#{class_name.capitalize} < ActiveRecord::Migration
+    def self.up	  
 END
    # add columns
   column_names = []
   number_columns = rand(4..6)
   
   number_columns.times do
-    files_with_types = correct ? 'words/falsedatatype.yaml' : 'qq.yaml'
-    hash = YAML::load(open(files_with_types))
     data_type = hash.keys.shuffle.first
     column_name =  (random_column_name).to_s
-	str << addcolumn(hash, column_name, table_name, data_type)
+	str << [addcolumn(hash, column_name, table_name, data_type), add_reference(class_names.join("\n").chop)].shuffle.first
 	column_names << column_name
     
   end
@@ -128,4 +72,5 @@ File.open('migration3.tex', 'a') do |file|
 	file.puts res.shuffle.join("\n")
 end
 end
+downrename
 
