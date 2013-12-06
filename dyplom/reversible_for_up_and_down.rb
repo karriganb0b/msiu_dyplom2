43bb222fe0fb9336971db69b2285f216a60b6292
Alexandr <slipfan4eg2@yandex.ru>
2013-12-06 09:53:25 +0400

    06.11.2013

--- dyplom/reversible_for_up_and_down.rb
+++ dyplom/reversible_for_up_and_down.rb
@@ -35,23 +35,24 @@ def index(table, correct=true, default)
 end
 def change_default(table, correct=true, change_default)
 
-  "\t\t\t\tt.change_default :#{table}, #{change_default}\n      end\n"
+  "\t\t\t\tt.change_default :#{table}, #{change_default}\n     end\n"
 end
 def change_column_up(table, name, data_type)
-"\t\t\t\tchange_column :#{table}, :#{name}, :#{data_type}\n       end\n"
+"\t\t\t\tchange_column :#{table}, :#{name}, :#{data_type}\n      \n"
 end
 def change_column_down(table, name, data_type)
-"\t\t\t\tchange_column :#{table}, :#{name}, :#{data_type}\n       end\n"
+"\t\t\t\tchange_column :#{table}, :#{name}, :#{data_type}\n       \n"
 end
-def change_column_null(table, name, rand_field)
-"t\t\t\t\tchange_column_null :#{table}, #{name},#{rand_field} \n      end\n"
+def change_column_null(table, name, null)
+"\t\t\t\tchange_column_null :#{table}, :#{name}, #{null}\n"
 end
 
 def random_migration(correct=false , correct_index=true, app) 
   # beginning of migration
+	
   str = <<-END
 class Create#{app.camelize} < ActiveRecord::Migration
-	 def change
+	 def up
 	   create_table :#{app} do |t|
 END
 
@@ -65,63 +66,97 @@ END
 	rand_fields = []
 	test = []
   number_columns = rand(4..6) 
-  
+
   number_columns.times do
-	data_type = hash.keys.shuffle.first
+	jk = hash.keys.shuffle
+	data_type = jk.pop
   method = hash[data_type]
   rand_field = rand_mod2(data_type) 
 	ins = send method
 
   column_name = correct ? (random_false_column).to_s : (random_column_name).to_s
 	defaultes << ins
-	ik = column(column_name, ins, rand_field, data_type)
-	str << ik
+	data_types << data_type
+   ik = column(column_name, ins, rand_field, data_type)
+	str <<  ik
 	test << ik
 	column_names << column_name
 	methodes << eval(method)
-	data_types << data_type
+	
   end
-	k = column_names.shuffle
-	for_sql = k.first
-  str << <<-END
-	t.references :#{for_sql}
-	end
+
+	str << <<-END
+		end
 	END
   # add indexes
   array = []
 	change_default = []
 	change_column = []
+	change_null_up = []
+	change_null_down = []	
 	1.times do
 	new_type = hash.keys.shuffle.first
-	data_type = data_types.pop
 	column_name = column_names.pop
 	column_names = column_names
 	column_name2 = column_names.pop
+	column_names = column_names
+	column_name3 = column_names.pop
+	column_name4 = column_names.pop
+	finder2 = test.find_all{|x| x.include?(column_name3) == true}.join(" ")
+	k2 = finder2.scan(/null: false|null: true/).join('')
+	k3 = k2.gsub(/(null:+)/, '\\2')
+	p k2
+	reverse_k = ["true", "false"].shuffle.first
+	if k3 == " true" or k3 == " false"
+	c_hash = {change_column_null(app, column_name3, k3) => change_column_null(app, column_name3, reverse_k)}
+	p c_hash
+	meth = c_hash.keys.first
+	c_meth = c_hash[meth]
+	change_null_up << meth
+	change_null_down << c_meth
+	elsif k3.size == 0
+	array << "\t\t\t\tt.belongs_to :#{[column_name3, column_name2, column_name].shuffle.first}, polymorphic: true\n"
+	array
+	change_default
+	end
+
+	dt = data_types.pop
+	data_types = data_types
+	
 	defaulte = defaultes.pop
+	defaultes = defaultes
 	methode = methodes.pop
-	finder = test.find_all{|x| x.include?(column_name2) == true}.join(" ")
-  finder = finder.scan(/integer|decimal|boolean|timestamp|string|date|time|time|text|float/).join(" ")
+	methodes = methodes
+
+finder = test.find_all{|x| x.include?(column_name2) == true}.join(" ")
+finder = finder.scan(/integer|decimal|boolean|timestamp|string|date|time|time|text|float/).join(" ")
+
+
+
 	array << index(column_name, column_name, defaulte)
 	change_default << change_default(column_name, column_name2, methode)
 	change_default << change_column_down(app, column_name2, new_type)
 	array << change_column_up(app, column_name2, finder)
-	
+	change_null_down << "\t\t\t\tremove_columns #{app} #{[" :#{column_name}"," :#{column_name2}"," :#{column_name3}"," :#{column_name4}"].shuffle.first}\n"
+
+	p new_type
   end
+
 
 	tab = app
 	
 	str << <<-END
-		reversible do |dir|
-      			dir.up do
 				change_table :#{app} do |t|
 	END
   str << array.join(" ")
+	str << change_null_up.join(" ")	
 
   str << <<-END
-			dir.down do
+			def down
 				change_table :#{app} do |t|
   END
 	str << change_default.join(" ")
+	str << change_null_down.join(" ")
   
   # end of migration
   str << <<-END
@@ -137,7 +172,7 @@ correct_indexes = ([true]).shuffle
 res << random_migration(false, correct_indexes.pop, app)
 0.times {res << random_migration(false, correct_indexes.pop) }
 
-file = File.open("words/test_migration/test_true/1_2.rb", 'a'){ |file| file.puts res.shuffle.join("\n") }
+file = File.open("words/test_migration/test_true/1_1.rb", 'a'){ |file| file.puts res.shuffle.join("\n") }
 end
-1.times{
-add_sbor}
+
+add_sbor
